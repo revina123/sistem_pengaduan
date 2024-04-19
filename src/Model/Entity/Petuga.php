907d8ac54e1d2aea05addb5c1513a2ee019e05b4
revina123 <revinasfr@gmail.com>
2024-04-19 14:38:54 +0700

    aplikasi pengaduan masyarakat

---
+++ src/Model/Entity/Petuga.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+use Authentication\PasswordHasher\DefaultPasswordHasher; // Add this line
+
+/**
+ * Petuga Entity
+ *
+ * @property int $id_petugas
+ * @property string $nik
+ * @property string $nama
+ * @property string $username
+ * @property string $password
+ * @property string $telp
+ * @property string $level
+ */
+class Petuga extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected array $_accessible = [
+        'nik' => true,
+        'nama' => true,
+        'username' => true,
+        'password' => true,
+        'telp' => true,
+        'level' => true,
+    ];
+
+    /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var list<string>
+     */
+    protected function _setPassword(string $password) : ?string
+    {
+        if (strlen($password) > 0) {
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+    }
+
+}
